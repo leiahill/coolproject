1aa69c446772794b7eaf0f7872f166d0a0fb73df
Leia Hill <leia.hill@g.austincc.edu>
2018-05-09 23:10:52 -0500

    servo files

---
+++ servo/main.cpp
@@ -0,0 +1,28 @@
+#include "Servo.h"
+#include "ServoTimers.h"
+
+Servo servo1;
+
+int pos = 0;
+
+void setup() {    
+    servo1.attach(9);
+}
+
+void loop() {
+    for(int pos = 95; pos <= 180; pos++) {
+        servo1.write(pos);
+    }
+    for(int pos = 180; pos >= 95; pos--) {
+        servo1.write(pos);
+    }
+}
+
+int main(void) {
+    setup();
+    for(;;) {
+	loop();
+    }
+    
+    return 0;
+}
